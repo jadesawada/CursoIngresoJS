ad57919c5d6ff663f6d2390af6649ac6e5ca1554
jadesawada <52410259+jadesawada@users.noreply.github.com>
2019-07-19 12:13:21 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,44 @@
 function mostrar()
 {
 
+
+  var mensaje;
+
+  planeta=prompt ("ingrese un planeta");
+
+  switch (planeta){
+
+    case "tierra":
+        mensaje = "aca vivimos";
+          break;
+
+
+    case "mercurio":
+
+    case "venus":
+         mensaje = ""
+          break;
+
+
+    case "marte":
+
+    case "jupiter":
+
+    case "saturno":
+
+    case "urano":
+
+    case "neptuno":
+          alert("aca hace mas frio");
+          break;
+
+
+    case "pluton":
+         alert("No es un planeta valido");
+          break;
+
+
+
+
+  }
 }
