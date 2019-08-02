1af201e3a338c03cc021b521359f0ba7df931d00
jadesawada <52410259+jadesawada@users.noreply.github.com>
2019-08-02 12:21:16 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
+    var nota;
+    var promedio;
+    var sexo;
+    var contador=0;
+
+    do{
+    
+        nota = prompt ("ingrese su nota entre 1 y 10");
+    
+        sexo = prompt ("ingrese f o m");
+
+        contador = contador + 1;
+
+        if()
+
+}  while (contador < 5)
+
+    
+
+
+
 
 }
