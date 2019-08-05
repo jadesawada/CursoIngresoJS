dd10efdd585368ede5ad47d5b4a800f9f5939d35
jadesawada <52410259+jadesawada@users.noreply.github.com>
2019-08-04 23:45:31 -0300

    Add files via upload

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,6 +6,16 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    var largo;
+    var ancho;
+    var resulado;
+
+    largo=document.getElementById("Largo").value;
+    ancho=document.getElementById("Ancho").value;
+
+    resultado = parseInt (largo) * parseInt(ancho);
+
+    alert ("el resultado es " + resultado);
 
 }
 function Circulo () 
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,7 +5,20 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    var precio1;
+    var precio2;
+    var precio3;
+    var suma;
+
+    precio1=document.getElementById("precioUno").value;
+    precio2=document.getElementById("precioDos").value;
+    precio3=document.getElementById("precioTres").value;
+
+    suma=parseInt (precio1) +parseInt (precio2) +parseInt (precio3);
+
+    alert("Suma de precios :" +suma);
+
+
 }
 function Promedio () 
 {
